b2898bcdbf67618bd6c4942ba9ef37c1eb8de076
Mikhail Andreev (adw0rd) <x11org@gmail.com>
2013-01-30 23:33:42 +0400

    Fixed: TorrentStream Error: BDecode failed (Errcode: 0)

--- torrent_stream/templatetags/torrent_stream.py
+++ torrent_stream/templatetags/torrent_stream.py
@@ -2,7 +2,7 @@ from django import template
 
 from ..helpers import get_content_id
 from ..settings import TORRENT_STREAM_PLAYER
-from ..exceptions import ServerFault
+from ..exceptions import FailedResponse, ServerFault
 
 register = template.Library()
 
@@ -15,7 +15,7 @@ def receive_content_id(torrent_file):
         content = torrent_file.read()
     try:
         return get_content_id(content)
-    except ServerFault:
+    except (FailedResponse, ServerFault):
         return None
 
 
